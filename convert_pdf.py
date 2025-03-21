076ababe93bed9dd9c3b0914b54bcbb1502e1b09
vinay babu <theautomationtester@hotmail.com>
2025-03-21 23:48:22 +0530

    htmltopdfready

--- convert_pdf.py
+++ convert_pdf.py
@@ -24,8 +24,11 @@
 import asyncio
 import os
 from pyppeteer import launch
-
+import pyppeteer
 import base64
+
+rev = pyppeteer.__chromium_revision__
+print(rev)
 
 base64_string = ""
 
@@ -35,8 +38,10 @@ with open("logo.png", "rb") as image_file:
 async def main():
     multiline_string = "I will  there."
     
-    head_template = "header"
+    #head_template = '<style>@divHeader{width: 2500px;height:auto;align-items:center;border-bottom:2px solid #413a97;} #logo{max-width:100px;} #tcid{margin-left:auto;margin-right:0;}</style><div id="divHeader"><img id="logo"src="data:image/png;base64,'+base64_string+'"alt="Logo"><hr></hr></div>'
+    head_template = '<div style="display: flex; width: 100%; height: auto; justify-content: space-between; align-items: center; border-bottom: 2px solid #413a97;"><div style="flex: 0;"><img id="logo" src="data:image/png;base64,'+base64_string+'" alt="Logo" style="max-width: 150px;"></div><div id="tcid" style="flex: 0;font-size:10px;white-space: nowrap;margin-right: 20px;"><h1>TCID-11221121</h1></div></div>'
     browser = await launch({"headless": True})
+    print(await browser.version())
     page = await browser.newPage()
     
     # Replace this with the path to your local HTML file
@@ -48,38 +53,17 @@ async def main():
     # Set PDF options for A4 size
     await page.pdf({
         "path": "output.pdf",
-        "format": "A4",
+        "format": "Letter",
         "printBackground": True,  # Include background graphics
         "landscape": False,
         "margin": {
-            "top": 70,
+            "top": 106,
             "bottom": 25,
-            "left": 35,
-            "right": 30
+            "left": 25,
+            "right": 25
         },
         "displayHeaderFooter": True,
-        "headerTemplate": """
-            <style>
-                #divHeader {
-                    display: flex;
-                    height: auto;
-                    justify-content: space-between;
-                    align-items:center;            
-                    border-bottom: 2px solid #413a97;
-                }
-                #logo {        
-                    max-width:150px;
-                }
-                #tcid 
-                    font-size: 10px;
-                    margin-left: auto;
-                    margin-right: 0;
-                }
-            </style>
-            <header id="divHeader">
-                <img id="logo" src='data:image/png;base64,{base64_string}' alt="Logo">
-                <h1 id = "tcid">TCID-11221121</h1>
-            </header>""",
+        "headerTemplate": head_template,
         "footerTemplate": """
             <div style='font-size:10px; width:100%; text-align:center;'>
                 Page <span class="pageNumber"></span> of <span class="totalPages"></span>
